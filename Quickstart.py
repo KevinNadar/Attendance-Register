72aa46d12203dccc9f6021b8aaab03e3aca1f4c1
Kevin Nadar <kevinxaviernadar@gmail.com>
2021-06-28 12:31:03 +0530

    Initial Commit

---
+++ Quickstart.py
@@ -0,0 +1,78 @@
+# %% Import Libraries
+from __future__ import print_function
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# %% Define Scopes
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.events.readonly',
+    'https://www.googleapis.com/auth/spreadsheets']
+
+
+""" # The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E' """
+
+# %% Define the function main
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    google_calendar = build('calendar', 'v3', credentials=creds)
+    google_sheets = build('sheets', 'v4', credentials=creds)
+
+    #Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = google_calendar.events().list(calendarId='primary', timeMin=now,
+        maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+    # Call the Sheets API
+    sheet = google_sheets.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+    return sheet
+
+
+
+if __name__ == '__main__':
+    output = main()
---
+++ README.md
@@ -0,0 +1,7 @@
+# Google Meet Attendance Register
+
+This project aims to simplify the attendance taking procedure in online lectures
+
+> API used in this project:
+1. Google Calendar API
+2. Google Sheets API
